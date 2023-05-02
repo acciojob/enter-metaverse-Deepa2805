d1869d67bda29af92b03e83d76b8d550a539a7aa
Dipmala Pal <121039724+Deepa2805@users.noreply.github.com>
2023-05-02 09:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let p = document.getElementById('status');
+let body = documemt.querySelector('body');
+let btn = document.getElementById('enterBtn');
+
+btn.addEventListener('click', (event) => {
+      p.innerHTML = "";
+      let h1 = document.createElement('h1');
+	  h1.innerHTML = "Entered Metaverse";
+	  p.appendChild(h1);	
+});
